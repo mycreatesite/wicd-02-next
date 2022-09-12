eb4524036f7fc283d2c09d360ac73f0fd57e8d8c
ma-ya's create <hello.myscreate@gmail.com>
2022-09-13 04:10:51 +0900

    feat: first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import 'modern-css-reset/dist/reset.min.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
