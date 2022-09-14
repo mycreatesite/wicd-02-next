e9bca12fd6357e15d037494b935a5460a2efab75
ma-ya's create <hello.myscreate@gmail.com>
2022-09-15 01:05:01 +0900

    feat: seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 import Layout from 'components/layout'
 import { useEffect } from 'react'
 import { AnimatePresence } from "framer-motion"
+import { DefaultSeo } from 'next-seo';
 
 if (typeof window !== "undefined") {
 
@@ -25,11 +26,38 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   },[router.pathname])
 
   return (
-    <AnimatePresence>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AnimatePresence>
+    <>
+      <DefaultSeo
+        defaultTitle="WHAT I CAN DO."
+        description="My name is Masayoshi Kawashima,as known as ma-ya's CREATE."
+        openGraph={{
+          type: "website",
+          title: "WHAT I CAN DO.",
+          description: "My name is Masayoshi Kawashima,as known as ma-ya's CREATE.",
+          site_name: "WHAT I CAN DO.",
+          url: "https://wicd-02-next.vercel.app/",
+          images: [
+            {
+              url: "https://wicd-02-next.vercel.app/ogp.png",
+              width: 1200,
+              height: 630,
+              alt: 'WHAT I CAN DO.',
+              type: 'image/png',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@hello_myscreate',
+          site: '@hello_myscreate',
+          cardType: "summary_large_image",
+        }}
+			/>
+      <AnimatePresence>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AnimatePresence>
+    </>
   )
 }
 
