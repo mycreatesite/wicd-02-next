44b808e97eef3ba5c916f73ce74d786e894f06bd
ma-ya's create <hello.myscreate@gmail.com>
2022-09-17 10:49:26 +0900

    feat: icon

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,16 @@
 import styles from 'styles/footer.module.scss'
 import Container from 'components/container'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 const Footer = () => {
   return (
     <footer className={`${styles.footer} footer`}>
       <Container>
         <nav className={styles.nav}>
-          <p>© Copyright 2022 │ ma-ya&apos;s CREATE All rights reserved.</p>
-          <a className={'hover-text-translation'} href="https://myscreate.com/" target="_blank" rel="noopener noreferrer"><span data-text="GO TO MAIN WEBSITE">GO TO MAIN WEBSITE</span></a>
+          <p className={styles.copy}>© Copyright 2022 │ ma-ya&apos;s CREATE All rights reserved.</p>
+          <a className={`${styles.link} hover-text-translation`} href="https://myscreate.com/" target="_blank" rel="noopener noreferrer">
+            <span data-text="GO TO MAIN WEBSITE">GO TO MAIN WEBSITE</span>
+            <OpenInNewIcon />
+          </a>
         </nav>
       </Container>
     </footer>
